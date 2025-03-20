9721358f3181ae6810489fb461e6854f3324a046
Julio <julioworkwith@gmail.com>
2025-03-20 22:55:55 +0700

    update (?)

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+/// <reference types="@workadventure/iframe-api-typings" />
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+
+WA.onInit().then(async () => {
+    console.log('loading main.ts');
+    try {
+        // Initialize the Scripting API Extra
+        await bootstrapExtra();
+        console.log('Scripting API Extra ready');
+    } catch (e) {
+        console.error(e);
+    }})
+    
+
+export {};
+
